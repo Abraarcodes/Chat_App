bec5c0df7bf205743b6d1dee04b4757262957b9c
Abraarcodes <mohammedabraar360@gmail.com>
2025-08-01 18:56:37 +0400

    Add message model

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import cors from 'cors'
 import http from 'http'
 import { connectDB } from './lib/db.js';
+import userRouter from './routes/userRoutes.js';
 
 //Create Express app and Http Server
 const app=express();
@@ -12,8 +13,13 @@ const server=http.createServer(app)
 app.use(express.json({limit: "4mb"}));
 app.use(cors())
 
-app.use('/api/status',(req,res)=>res.send("Server is live"))
 
+//Routes setup
+app.use('/api/status',(req,res)=>res.send("Server is live"))
+app.use("/api/auth",userRouter);
+
+
+ 
 //connect to mongodb
 await connectDB()
 const PORT=process.env.port || 5000
