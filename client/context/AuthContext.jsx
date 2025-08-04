7f5cfcd0e04346340327e79a1179682de15bf76b
Abraarcodes <mohammedabraar360@gmail.com>
2025-08-04 13:48:36 +0400

    Add profile update function

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -67,6 +67,22 @@ export const AuthProvider=({children})=>{
         socket.disconnect();
     }
 
+
+    //Update profile function to handle user profile updates
+
+    const updateProfile=async(body)=>{
+        try{
+            const {data}=await axios.put('/api/auth/update-profile',body);
+            if(data.success){
+                setAuthUser(data.user);
+                toast.success("Profile updated successfully");
+            }
+        }catch(error){
+            toast.error(error.message)
+        }
+    }
+
+
     //Connect socket function to handle socket connection and online users updates
 
     const connectSocket=(userData)=>{
@@ -93,7 +109,10 @@ export const AuthProvider=({children})=>{
         axios,
         authUser,
         onlineUsers,
-        socket
+        socket,
+        login,
+        logout,
+        updateProfile
     }
 
 
