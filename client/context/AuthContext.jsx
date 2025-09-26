6ea1725ca3ed12af32c513765a872ebdfe0842b2
Abraarcodes <mohammedabraar360@gmail.com>
2025-09-27 01:23:10 +0530

    Add backend url

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 import { useEffect } from "react";
 import {io} from 'socket.io-client';
 
-const backendUrl=import.meta.env.VITE_BACKEND_URL;
+const backendUrl=import.meta.env.VITE_BACKEND_URL || 'https://chat-app-backend-2z3z.onrender.com/';
 axios.defaults.baseURL=backendUrl;
 
 
